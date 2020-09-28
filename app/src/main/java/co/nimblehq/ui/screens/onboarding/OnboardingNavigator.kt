f1473784a534c0d4331884a5687d328e15937e89
Minh Pham <minh@nimblehq.co>
2020-09-28 17:35:12 +0700

    Move activity navigation to Navigator class

--- app/src/main/java/co/nimblehq/ui/screens/onboarding/OnboardingNavigator.kt
+++ app/src/main/java/co/nimblehq/ui/screens/onboarding/OnboardingNavigator.kt
@@ -1,15 +1,18 @@
 package co.nimblehq.ui.screens.onboarding
 
 import android.app.Activity
+import android.content.Intent
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import co.nimblehq.R
 import co.nimblehq.ui.base.BaseNavigator
 import co.nimblehq.ui.base.BaseNavigatorImpl
+import co.nimblehq.ui.screens.main.MainActivity
 import javax.inject.Inject
 
 interface OnboardingNavigator : BaseNavigator {
-
+    fun navigateToMainActivity()
 }
 
 class OnboardingNavigatorImpl @Inject constructor(
@@ -18,5 +21,17 @@ class OnboardingNavigatorImpl @Inject constructor(
 
     override fun findNavController(): NavController {
         return activity.findNavController(R.id.navHostContainer)
+    }
+
+    override fun navigateToMainActivity() {
+        val activityNavigator = ActivityNavigator(activity)
+        activityNavigator.navigate(
+            activityNavigator.createDestination().setIntent(
+                Intent(
+                    activity,
+                    MainActivity::class.java
+                )
+            ), null, null, null
+        )
     }
 }
