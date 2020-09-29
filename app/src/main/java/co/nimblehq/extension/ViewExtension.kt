b47717840b3d16b955dba5292e62da33c08da34c
Minh Pham <minh@nimblehq.co>
2020-09-29 14:49:39 +0700

    Fix indentations

--- app/src/main/java/co/nimblehq/extension/ViewExtension.kt
+++ app/src/main/java/co/nimblehq/extension/ViewExtension.kt
@@ -8,23 +8,23 @@ import co.nimblehq.data.lib.common.Const
 fun View.startFadeInAnimation(shouldAnimate: Boolean = true, executeOnAnimationEnd: (() -> Unit)? = null) {
     alpha = 0f
     animate()
-            .alpha(1f)
-            .setDuration(if (shouldAnimate) Const.Animation.DURATION else 0)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    executeOnAnimationEnd?.invoke()
-                }
-            })
+        .alpha(1f)
+        .setDuration(if (shouldAnimate) Const.Animation.DURATION else 0)
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                executeOnAnimationEnd?.invoke()
+            }
+        })
 }
 
 fun View.startFadeOutAnimation(executeOnAnimationEnd: (() -> Unit)? = null) {
     alpha = 0f
     animate()
-            .alpha(1f)
-            .setDuration(Const.Animation.DURATION)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    visibility = View.GONE
-                }
-            })
+        .alpha(1f)
+        .setDuration(Const.Animation.DURATION)
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                visibility = View.GONE
+            }
+        })
 }
--- app/src/main/java/co/nimblehq/extension/ViewGroupExtension.kt
+++ app/src/main/java/co/nimblehq/extension/ViewGroupExtension.kt
@@ -7,10 +7,10 @@ import jp.wasabeef.blurry.Blurry
 fun ViewGroup.addBlurWithAnimation(shouldAnimate: Boolean = true, shouldPost: Boolean = false) {
     fun addBlur() {
         Blurry.with(context)
-                .radius(25)
-                .sampling(2)
-                .animate(if (shouldAnimate) Const.Animation.DURATION.toInt() else 0)
-                .onto(this)
+            .radius(25)
+            .sampling(2)
+            .animate(if (shouldAnimate) Const.Animation.DURATION.toInt() else 0)
+            .onto(this)
     }
 
     if (shouldPost) this.post { addBlur() } else addBlur()
