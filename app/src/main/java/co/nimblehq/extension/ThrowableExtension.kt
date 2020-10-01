638e3f39d30c83193cf6f7b0b10978ec31e22e49
Minh Pham <minh@nimblehq.co>
2020-10-01 13:10:44 +0700

    Write unit test for extension files
    (cherry picked from commit f819d2f4c6e83c7438a4dcd63336983e3c1fa338)

--- app/src/main/java/co/nimblehq/extension/ThrowableExtension.kt
+++ app/src/main/java/co/nimblehq/extension/ThrowableExtension.kt
@@ -2,7 +2,13 @@ package co.nimblehq.extension
 
 import android.content.Context
 import co.nimblehq.R
+import co.nimblehq.data.error.AppError
 
 fun Throwable.userReadableMessage(context: Context): String {
-    return context.getString(R.string.generic_error) // TODO: Add more error messages handler here
+    return when (this) {
+        is AppError -> {
+            readableMessage ?: context.getString(readableMessageRes ?: R.string.generic_error)
+        }
+        else -> context.getString(R.string.generic_error)
+    }
 }
---
+++ app/src/test/java/co/nimblehq/extension/ViewExtensionKtTest.kt
@@ -0,0 +1,61 @@
+package co.nimblehq.extension
+
+import android.content.Context
+import android.view.View
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Test
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@Suppress("IllegalIdentifier")
+@RunWith(RobolectricTestRunner::class)
+class ViewExtensionKtTest {
+
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Test
+    fun `using startFadeInAnimation should initially set alpha to 0`() {
+
+        // Arrange
+        val view = View(context)
+
+        // Act
+        view.startFadeInAnimation()
+
+        // Assert
+        assertEquals("alpha should be initially equal 0f", 0f, view.alpha)
+    }
+
+    @Test
+    fun `using startFadeInAnimation should trigger action and update alpha to 1 when completed`() {
+
+        // Arrange
+        val view = View(context)
+        var result = 5
+
+        // Act
+        view.startFadeInAnimation {
+            result = 6
+
+            // Assert
+            assertEquals("completion block should exec when completed", 6, result)
+            assertEquals("alpha should be updated to 1f when completed", 1f, view.alpha)
+        }
+    }
+
+    @Test
+    fun `using startFadeInAnimation with shouldAnimate = false should set view's alpha to 1f in the next runloop`() {
+
+        // Arrange
+        val view = View(context)
+
+        // Act
+        view.startFadeInAnimation(shouldAnimate = false)
+
+        // Assert
+        view.post {
+            assertEquals("alpha should be equal 1f in the next runloop", 1f, view.alpha)
+        }
+    }
+}
