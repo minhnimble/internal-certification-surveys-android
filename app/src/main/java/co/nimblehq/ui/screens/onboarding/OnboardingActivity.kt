42b38308c31ae7f70efa3366ac96c888565589a6
Minh Pham <minh@nimblehq.co>
2020-10-01 09:23:35 +0700

    Apply best practice for communication between fragment and activity Remove action bar

--- app/src/main/java/co/nimblehq/ui/screens/onboarding/OnboardingActivity.kt
+++ app/src/main/java/co/nimblehq/ui/screens/onboarding/OnboardingActivity.kt
@@ -2,24 +2,29 @@ package co.nimblehq.ui.screens.onboarding
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import co.nimblehq.R
 import co.nimblehq.extension.addBlurWithAnimation
 import co.nimblehq.ui.base.BaseActivity
+import co.nimblehq.ui.screens.onboarding.signin.SigninFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
-interface BlurAnimatable {
-    fun animateBlurBackground()
-}
-
 @AndroidEntryPoint
-class OnboardingActivity : BaseActivity(), BlurAnimatable {
+class OnboardingActivity : BaseActivity(), SigninFragment.BlurAnimatable {
 
     private val viewModel by viewModels<OnboardingViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+        if (fragment is SigninFragment) {
+            fragment.setBlurAnimator(this)
+        }
     }
 
     override fun animateBlurBackground() {
