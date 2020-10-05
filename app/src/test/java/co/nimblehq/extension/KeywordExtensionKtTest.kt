bbf753f4b7c503c67e0357637f8680e95c199a9b
Minh Pham <minh@nimblehq.co>
2020-10-05 16:14:38 +0700

    Add unit tests for login feature

---
+++ app/src/test/java/co/nimblehq/extension/KeywordExtensionKtTest.kt
@@ -0,0 +1,36 @@
+package co.nimblehq.extension
+
+import org.junit.Test
+import org.junit.Assert.*
+
+@Suppress("IllegalIdentifier")
+class KeywordExtensionKtTest {
+
+    @Test
+    fun `using unless should not execute if the condition is true`() {
+
+        // Arrange
+        var result = 3
+        val condition: Int = -1
+
+        // Act
+        unless(condition > -1, { result = 4})
+
+        // Assert
+        assertEquals("block should exec", 4, result)
+    }
+
+    @Test
+    fun `using unless should execute if the condition is false`() {
+
+        // Arrange
+        var result = 3
+        val condition: Int = -1
+
+        // Act
+        unless(condition == -1, { result = 5})
+
+        // Assert
+        assertEquals("block should NOT exec", 3, result)
+    }
+}
--- app/src/test/java/co/nimblehq/ui/screen/onboarding/signin/SignInViewModelTest.kt
+++ app/src/test/java/co/nimblehq/ui/screen/onboarding/signin/SignInViewModelTest.kt
@@ -1,3 +1,140 @@
 package co.nimblehq.ui.screen.onboarding.signin
 
-class SignInViewModelTest { }
+import co.nimblehq.data.error.LoginError
+import co.nimblehq.usecase.session.LoginByPasswordCompletableUseCase
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Completable
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+
+class SignInViewModelTest {
+
+    private lateinit var mockLoginByPasswordCompletableUseCase: LoginByPasswordCompletableUseCase
+
+    private lateinit var signingViewModel: SignInViewModel
+
+    @Before
+    fun setUp() {
+        mockLoginByPasswordCompletableUseCase = mock()
+        signingViewModel = SignInViewModel(mockLoginByPasswordCompletableUseCase)
+    }
+
+    @Test
+    fun `When input wrong email and password combination, it returns LoginError`() {
+        // Arrange
+        whenever(
+            mockLoginByPasswordCompletableUseCase.execute(any())
+        ) doReturn Single.error<LoginError>(LoginError(null)).ignoreElement()
+
+        // Act
+        val testSubscriber = signingViewModel
+            .login("invalid@nimblehq.co", "12345678")
+            .test()
+
+        // Assert
+        testSubscriber
+            .assertError { error ->
+                error is LoginError
+            }
+            .assertValueCount(0)
+    }
+
+    @Test
+    fun `When input incorrect email format, Login button is disabled`() {
+        // Arrange
+        val enableLoginButtonObserver = signingViewModel
+            .enableLoginButton
+            .test()
+
+        // Act
+        signingViewModel.inputs.updateEmail("acbd")
+        signingViewModel.inputs.updatePassword("12345678")
+
+        // Assert
+        enableLoginButtonObserver
+            .assertNoErrors()
+            .assertValueAt(0) {
+                !it
+            }
+            .assertValueAt(1) {
+                !it
+            }
+            .assertValueAt(2) {
+                !it
+            }
+    }
+
+    @Test
+    fun `When input empty password, Login button is disabled`() {
+        // Arrange
+        val enableLoginButtonObserver = signingViewModel
+            .enableLoginButton
+            .test()
+
+        // Act
+        signingViewModel.inputs.updateEmail("test@nimblehq.co")
+        signingViewModel.inputs.updatePassword("")
+
+        // Assert
+        enableLoginButtonObserver
+            .assertNoErrors()
+            .assertValueAt(0) {
+                !it
+            }
+            .assertValueAt(1) {
+                !it
+            }
+            .assertValueAt(2) {
+                !it
+            }
+    }
+
+    @Test
+    fun `When input valid email and password, Login button is enable`() {
+        // Arrange
+        val enableLoginButtonObserver = signingViewModel
+            .enableLoginButton
+            .test()
+
+        // Act
+        signingViewModel.inputs.updateEmail("test@nimblehq.co")
+        signingViewModel.inputs.updatePassword("12345678")
+
+        // Assert
+        enableLoginButtonObserver
+            .assertNoErrors()
+            .assertValueAt(0) {
+                !it
+            }
+            .assertValueAt(1) {
+                !it
+            }
+            .assertValueAt(2) {
+                it
+            }
+    }
+
+    @Test
+    fun `When logging in completed, loading progress is gone`() {
+        // Arrange
+        whenever(
+            mockLoginByPasswordCompletableUseCase.execute(any())
+        ) doReturn Completable.complete()
+
+        // Act
+        signingViewModel
+            .login("test@nimblehq.co", "12345678")
+            .test()
+
+        val showLoadingObserver = signingViewModel.showLoading.test()
+
+        showLoadingObserver
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValue(false)
+    }
+}
