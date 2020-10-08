a6decf16966ed59ec8d9d9af0f455d078b691462
Minh Pham <minh@nimblehq.co>
2020-10-08 15:15:02 +0700

    Refactor code

--- app/src/main/java/co/nimblehq/event/NavigationEvent.kt
+++ app/src/main/java/co/nimblehq/event/NavigationEvent.kt
@@ -2,6 +2,10 @@ package co.nimblehq.event
 
 sealed class NavigationEvent {
 
+    sealed class Onboarding : NavigationEvent() {
+        object Main : Onboarding()
+    }
+
 	sealed class SignIn : NavigationEvent() {
 		object Main : SignIn()
 	}
