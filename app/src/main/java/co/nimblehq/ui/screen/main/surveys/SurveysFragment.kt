03e01fc6f5b51eab25391b08d28b7dabcf89cc03
Minh Pham <minh@nimblehq.co>
2020-10-09 12:48:43 +0700

    Add shimmer loading indicator

--- app/src/main/java/co/nimblehq/ui/screen/main/surveys/SurveysFragment.kt
+++ app/src/main/java/co/nimblehq/ui/screen/main/surveys/SurveysFragment.kt
@@ -1,9 +1,11 @@
 package co.nimblehq.ui.screen.main.surveys
 
+import android.os.Handler
 import androidx.fragment.app.viewModels
 import co.nimblehq.R
 import co.nimblehq.ui.base.BaseFragment
 import co.nimblehq.ui.base.BaseFragmentCallbacks
+import co.nimblehq.ui.screen.main.LoaderAnimatable
 import co.nimblehq.ui.screen.main.MainNavigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -15,11 +17,20 @@ class SurveysFragment: BaseFragment(), BaseFragmentCallbacks {
 
     private val viewModel by viewModels<SurveysViewModel>()
 
+    private val loaderAnimator: LoaderAnimatable? by lazy {
+        requireActivity() as? LoaderAnimatable
+    }
+
     override val layoutRes = R.layout.fragment_surveys
 
     override fun initViewModel() { }
 
-    override fun setupView() { }
+    override fun setupView() {
+        // TODO: Will remove when implement with load surveys list logic
+        Handler().postDelayed({
+            loaderAnimator?.toggleShimmerLoader(false)
+        }, 2000)
+    }
 
     override fun bindViewEvents() { }
 
