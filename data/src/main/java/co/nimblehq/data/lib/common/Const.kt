e609275f430bbe98c2aa46f3c8ba2f8735a16d07
Minh Pham <minh@nimblehq.co>
2020-10-14 10:40:57 +0700

    Refactor constants

--- data/src/main/java/co/nimblehq/data/lib/common/Const.kt
+++ data/src/main/java/co/nimblehq/data/lib/common/Const.kt
@@ -5,7 +5,6 @@ package co.nimblehq.data.lib.common
 const val DEFAULT_DURATION = 1000L // in milliseconds
 
 const val BLUR_RADIUS = 25
-
 const val BLUR_SAMPLING = 2
 
 // Api related
@@ -13,22 +12,17 @@ const val BLUR_SAMPLING = 2
 const val AUTHORIZATION_HEADER = "Authorization"
 
 const val DEFAULT_INITIAL_SURVEYS_PAGE_NUMBER = 1
-
 const val DEFAULT_SURVEYS_PAGE_SIZE = 10
 
 const val OAUTH_GRANT_TYPE_PASSWORD = "password"
-
 const val OAUTH_GRANT_TYPE_REFRESH_TOKEN = "refresh_token"
+
+internal const val REQUEST_KEY_PAGE_NUMBER = "page[number]"
+internal const val REQUEST_KEY_PAGE_SIZE = "page[size]"
 
 // Date formats
 
 const val DATE_FORMAT_FULL_DISPLAY = "dd MMM yyyy, hh:mm a"
-
 const val DATE_FORMAT_SHORT_DISPLAY = "EEEE, MMMM dd"
 
-// Request keys
-
-internal const val REQUEST_KEY_PAGE_NUMBER = "page[number]"
-
-internal const val REQUEST_KEY_PAGE_SIZE = "page[size]"
 
