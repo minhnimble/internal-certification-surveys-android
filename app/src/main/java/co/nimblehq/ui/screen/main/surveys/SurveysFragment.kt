2b3001b9b009ad1de4b27b1b5fd08651576ec108
Minh Pham <minh@nimblehq.co>
2020-10-16 10:58:23 +0700

    Implement right menu UI for surveys list screen

--- app/src/main/java/co/nimblehq/ui/screen/main/surveys/SurveysFragment.kt
+++ app/src/main/java/co/nimblehq/ui/screen/main/surveys/SurveysFragment.kt
@@ -1,8 +1,11 @@
 package co.nimblehq.ui.screen.main.surveys
 
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.viewModels
 import co.nimblehq.R
 import co.nimblehq.data.lib.common.DATE_FORMAT_SHORT_DISPLAY
+import co.nimblehq.data.lib.extension.subscribeOnClick
 import co.nimblehq.extension.toDisplayFormat
 import co.nimblehq.ui.base.BaseFragment
 import co.nimblehq.ui.base.BaseFragmentCallbacks
@@ -10,13 +13,15 @@ import co.nimblehq.ui.screen.main.LoaderAnimatable
 import co.nimblehq.ui.screen.main.MainNavigator
 import co.nimblehq.ui.screen.main.surveys.adapter.SurveysPagerAdapter
 import co.nimblehq.ui.screen.main.surveys.adapter.SurveysPagerItemUiModel
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.content_fragment_surveys.*
 import kotlinx.android.synthetic.main.fragment_surveys.*
 import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SurveysFragment: BaseFragment(), BaseFragmentCallbacks {
+class SurveysFragment: BaseFragment(), BaseFragmentCallbacks, NavigationView.OnNavigationItemSelectedListener {
 
     @Inject lateinit var navigator: MainNavigator
 
@@ -44,7 +49,12 @@ class SurveysFragment: BaseFragment(), BaseFragmentCallbacks {
         ciSurveysIndicator.setViewPager(vpSurveys)
     }
 
-    override fun bindViewEvents() { }
+    override fun bindViewEvents() {
+        nvDrawerContainer.setNavigationItemSelectedListener(this)
+        ivSurveysUserAvatar.subscribeOnClick {
+            toggleDrawer(!dlSurveys.isDrawerOpen(GravityCompat.END))
+        }.bindForDisposable()
+    }
 
     override fun bindViewModel() {
         viewModel.error
@@ -60,6 +70,17 @@ class SurveysFragment: BaseFragment(), BaseFragmentCallbacks {
             .bindForDisposable()
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_logout -> {
+                //TODO: Implement logout logic
+            }
+        }
+        toggleDrawer(false)
+        return true
+    }
+
     private fun bindLoading(isLoading: Boolean) {
         if (!isLoading) loaderAnimator?.toggleShimmerLoader(isLoading)
         toggleLoading(isLoading)
@@ -69,5 +90,9 @@ class SurveysFragment: BaseFragment(), BaseFragmentCallbacks {
         if (uiModels.isNotEmpty()) {
             surveysPagerAdapter.uiModels = uiModels
         }
+    }
+
+    private fun toggleDrawer(shouldShow: Boolean) {
+        if (shouldShow) dlSurveys.openDrawer(GravityCompat.END) else dlSurveys.closeDrawer(GravityCompat.END)
     }
 }
