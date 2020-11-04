32ba3b0268682e910743980c534ab602e3d1b002
Minh Pham <minh@nimblehq.co>
2020-11-04 16:41:01 +0700

    Added Logout ViewModel tests and UseCase tests

---
+++ app/src/test/java/co/nimblehq/usecase/session/LogoutCompletableUseCaseTest.kt
@@ -0,0 +1,62 @@
+package co.nimblehq.usecase.session
+
+import co.nimblehq.data.error.SessionError
+import co.nimblehq.data.lib.schedulers.TestRxSchedulerProviderImpl
+import co.nimblehq.data.repository.AuthRepository
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+
+class LogoutCompletableUseCaseTest {
+
+    private lateinit var mockRepository: AuthRepository
+    private lateinit var useCase: LogoutCompletableUseCase
+
+    @Before
+    fun setUp() {
+        mockRepository = mock()
+        useCase = LogoutCompletableUseCase(
+            TestRxSchedulerProviderImpl(),
+            mockRepository
+        )
+    }
+
+    @Test
+    fun `When logging out succeeds, it returns Complete`() {
+        // Arrange
+        whenever(
+            mockRepository.logout(any())
+        ) doReturn Completable.complete()
+
+        // Act
+        val positiveTestSubscriber = useCase
+            .execute("token")
+            .test()
+
+        // Assert
+        positiveTestSubscriber
+            .assertNoErrors()
+            .assertComplete()
+    }
+
+    @Test
+    fun `When logging in fails, it returns a LogoutError`() {
+        // Arrange
+        whenever(
+            mockRepository.logout(any())
+        ) doReturn  Completable.error(SessionError.LogoutError())
+
+        // Act
+        val negativeTestSubscriber = useCase
+            .execute("token")
+            .test()
+
+        // Assert
+        negativeTestSubscriber
+            .assertError { it is SessionError.LogoutError }
+    }
+}
