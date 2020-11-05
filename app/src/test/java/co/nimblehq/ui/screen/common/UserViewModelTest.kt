eabf6b1055477e9164dcba44820402267b08f29b
Minh Pham <minh@nimblehq.co>
2020-11-05 17:32:56 +0700

    Added Logout ViewModel tests and UseCase tests

---
+++ app/src/test/java/co/nimblehq/ui/screen/common/UserViewModelTest.kt
@@ -0,0 +1,71 @@
+package co.nimblehq.ui.screen.common
+
+import co.nimblehq.data.error.UserError
+import co.nimblehq.data.model.User
+import co.nimblehq.usecase.user.LoadCurrentUserInfoSingleUseCase
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+
+class UserViewModelTest {
+
+    private lateinit var mockLoadCurrentUserInfoSingleUseCase: LoadCurrentUserInfoSingleUseCase
+
+    private lateinit var userViewModel: UserViewModel
+
+    @Before
+    fun setUp() {
+        mockLoadCurrentUserInfoSingleUseCase = mock()
+    }
+
+    @Test
+    fun `When loading current user info failed, it triggers a GetCurrentUserInfoError`() {
+        // Arrange
+        whenever(
+            mockLoadCurrentUserInfoSingleUseCase.execute(any())
+        ) doReturn Single.error(UserError.LoadCurrentUserInfoError(null))
+
+        // Act
+        userViewModel = UserViewModel(mockLoadCurrentUserInfoSingleUseCase)
+        val errorObserver = userViewModel
+            .output
+            .error
+            .test()
+        userViewModel.loadCurrentUserInfo()
+
+        // Assert
+        errorObserver
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValue { it is UserError.LoadCurrentUserInfoError }
+    }
+
+    @Test
+    fun `When loading current user info successfully, it broadcast the User object to all places that are listening to it`() {
+        // Arrange
+        val user = User(id = "id", email = "minh@nimblehq.co", avatarUrl = "avatar")
+        whenever(
+            mockLoadCurrentUserInfoSingleUseCase.execute(any())
+        ) doReturn Single.just(user)
+
+        // Act
+        userViewModel = UserViewModel(mockLoadCurrentUserInfoSingleUseCase)
+        val userObserver = userViewModel
+            .output
+            .user
+            .test()
+        userViewModel.loadCurrentUserInfo()
+
+        // Assert
+        userObserver
+            .assertNoErrors()
+            .assertValueCount(1)
+            .assertValue {
+                it.email == user.email && user.avatarUrl == it.avatarUrl
+            }
+    }
+}
