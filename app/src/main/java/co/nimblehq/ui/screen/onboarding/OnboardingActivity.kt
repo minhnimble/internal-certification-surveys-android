56f5efd7eacdaf151f2c5e7e0e7e12f5ab018665
Minh Pham <minh@nimblehq.co>
2020-11-23 18:11:30 +0700

    Implement logic for refreshing token and retry if receiving token expired message from backend Implement logic for kicking user out of the current session if the app fails to refresh token when receiving token expired message from backend

--- app/src/main/java/co/nimblehq/ui/screen/onboarding/OnboardingActivity.kt
+++ app/src/main/java/co/nimblehq/ui/screen/onboarding/OnboardingActivity.kt
@@ -24,7 +24,6 @@ class OnboardingActivity : BaseActivity(), BlurAnimatable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_onboarding)
         bindViewModel()
     }
